acc24760a86feb4b6061cd24439ab83b27dfe68b
reffx <ripsli@hotmail.com>
2019-03-25 22:26:37 +0100

    first commit

---
+++ src/components/shared/models/User.js
@@ -0,0 +1,17 @@
+/**
+ * User model
+ */
+class User {
+  constructor(data = {}) {
+    this.id = null;
+    this.name = null;
+    this.username = null;
+    this.password = null;
+    this.token = null;
+    this.status = null;
+    this.games = null;
+    this.moves = null;
+    Object.assign(this, data);
+  }
+}
+export default User;
