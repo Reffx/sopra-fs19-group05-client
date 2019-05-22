6c532777e1a428f33b219c5cb4db83f8b35bf6ad
reffx <ripsli@hotmail.com>
2019-05-22 20:43:05 +0200

    G05-102 chooseGodeMode if 2 gods selected the others get black-transparent

--- src/components/lobby/ChooseGodCard.js
+++ src/components/lobby/ChooseGodCard.js
@@ -63,35 +63,26 @@ class ChooseGodCard extends React.Component {
         }, 1000)
     }
 
-    return_active_card(inactiveCard){
-        if(inactiveCard === "Apollo"){
+    return_active_card(inactiveCard) {
+        if (inactiveCard === "Apollo") {
             return "Apollo"
-        }
-        else if(inactiveCard === "InactiveArtemis"){
+        } else if (inactiveCard === "InactiveArtemis") {
             return "Artemis"
-        }
-        else if(inactiveCard === "Athena"){
+        } else if (inactiveCard === "Athena") {
             return "Athena"
-        }
-        else if(inactiveCard === "InactiveAtlas"){
+        } else if (inactiveCard === "InactiveAtlas") {
             return "Atlas"
-        }
-        else if(inactiveCard === "InactiveDemeter"){
+        } else if (inactiveCard === "InactiveDemeter") {
             return "Demeter"
-        }
-        else if(inactiveCard === "InactiveHephaestus"){
+        } else if (inactiveCard === "InactiveHephaestus") {
             return "Hephaestus"
-        }
-        else if(inactiveCard === "InactiveHermes"){
+        } else if (inactiveCard === "InactiveHermes") {
             return "Hermes"
-        }
-        else if(inactiveCard === "Minotaur"){
+        } else if (inactiveCard === "Minotaur") {
             return "Minotaur"
-        }
-        else if(inactiveCard === "Pan"){
+        } else if (inactiveCard === "Pan") {
             return "Pan"
-        }
-        else if(inactiveCard === "InactivePrometheus"){
+        } else if (inactiveCard === "InactivePrometheus") {
             return "Prometheus"
         }
     }
@@ -160,7 +151,7 @@ class ChooseGodCard extends React.Component {
             if (this.state.player_is_playing === this.state.player1) {
                 if (this.state.player1.worker1.godCard === "None") {
                     this.setGodCard(card, this.state.player1.id, inactiveCard)
-                } else if (this.state.player1.worker1.godCard !== "None" && this.return_active_card(this.state.player1.worker1.godCard) !==card){
+                } else if (this.state.player1.worker1.godCard !== "None" && this.return_active_card(this.state.player1.worker1.godCard) !== card) {
                     this.setGodCard(card, this.state.player2.id, inactiveCard)
                 }
             }
@@ -181,8 +172,7 @@ class ChooseGodCard extends React.Component {
         var tempCard;
         if (inactiveGodCard === "NONE") {
             tempCard = godCard;
-        } else
-        {
+        } else {
             tempCard = inactiveGodCard;
         }
         alert(tempCard);
@@ -217,88 +207,98 @@ class ChooseGodCard extends React.Component {
         }
     }
 
-    setSessionStorageCard(godCard, id){
-        if ((godCard === this.state.card1 || godCard === this.state.card1Inactive) && id === this.state.player1.id){
+    setSessionStorageCard(godCard, id) {
+        if ((godCard === this.state.card1 || godCard === this.state.card1Inactive) && id === this.state.player1.id) {
             sessionStorage.setItem("GodCardPlayer1Inactive", this.state.card1Inactive);
             sessionStorage.setItem("GodCardPlayer1", this.state.card1);
         }
-        if ((godCard === this.state.card2 || godCard === this.state.card2Inactive) && id === this.state.player1.id){
+        if ((godCard === this.state.card2 || godCard === this.state.card2Inactive) && id === this.state.player1.id) {
             sessionStorage.setItem("GodCardPlayer1Inactive", this.state.card2Inactive);
             sessionStorage.setItem("GodCardPlayer1", this.state.card2);
         }
-        if (((godCard === this.state.card3) || (godCard === "InactiveAthena")) && (id === this.state.player1.id)){
+        if (((godCard === this.state.card3) || (godCard === "InactiveAthena")) && (id === this.state.player1.id)) {
             sessionStorage.setItem("GodCardPlayer1Inactive", this.state.card3Inactive);
             sessionStorage.setItem("GodCardPlayer1", this.state.card3);
         }
-        if ((godCard === this.state.card4 || godCard === this.state.card4Inactive) && id === this.state.player1.id){
+        if ((godCard === this.state.card4 || godCard === this.state.card4Inactive) && id === this.state.player1.id) {
             sessionStorage.setItem("GodCardPlayer1Inactive", this.state.card4Inactive);
             sessionStorage.setItem("GodCardPlayer1", this.state.card4);
         }
-        if ((godCard === this.state.card5 || godCard === this.state.card5Inactive) && id === this.state.player1.id){
+        if ((godCard === this.state.card5 || godCard === this.state.card5Inactive) && id === this.state.player1.id) {
             sessionStorage.setItem("GodCardPlayer1Inactive", this.state.card5Inactive);
             sessionStorage.setItem("GodCardPlayer1", this.state.card5);
         }
-        if ((godCard === this.state.card6 || godCard === this.state.card6Inactive) && id === this.state.player1.id){
+        if ((godCard === this.state.card6 || godCard === this.state.card6Inactive) && id === this.state.player1.id) {
             sessionStorage.setItem("GodCardPlayer1Inactive", this.state.card6Inactive);
             sessionStorage.setItem("GodCardPlayer1", this.state.card6);
         }
-        if ((godCard === this.state.card7 || godCard === this.state.card7Inactive) && id === this.state.player1.id){
+        if ((godCard === this.state.card7 || godCard === this.state.card7Inactive) && id === this.state.player1.id) {
             sessionStorage.setItem("GodCardPlayer1Inactive", this.state.card7Inactive);
             sessionStorage.setItem("GodCardPlayer1", this.state.card7);
         }
-        if ((godCard === this.state.card8 || godCard === this.state.card8Inactive) && id === this.state.player1.id){
+        if ((godCard === this.state.card8 || godCard === this.state.card8Inactive) && id === this.state.player1.id) {
             sessionStorage.setItem("GodCardPlayer1Inactive", this.state.card8Inactive);
             sessionStorage.setItem("GodCardPlayer1", this.state.card8);
         }
-        if ((godCard === this.state.card9 || godCard === this.state.card9Inactive) && id === this.state.player1.id){
+        if ((godCard === this.state.card9 || godCard === this.state.card9Inactive) && id === this.state.player1.id) {
             sessionStorage.setItem("GodCardPlayer1Inactive", this.state.card9Inactive);
             sessionStorage.setItem("GodCardPlayer1", this.state.card9);
         }
-        if ((godCard === this.state.card10 || godCard === this.state.card10Inactive) && id === this.state.player1.id){
+        if ((godCard === this.state.card10 || godCard === this.state.card10Inactive) && id === this.state.player1.id) {
             sessionStorage.setItem("GodCardPlayer1Inactive", this.state.card10Inactive);
             sessionStorage.setItem("GodCardPlayer1", this.state.card10);
         }
-        if ((godCard === this.state.card1 || godCard === this.state.card1Inactive) && id === this.state.player2.id){
+        if ((godCard === this.state.card1 || godCard === this.state.card1Inactive) && id === this.state.player2.id) {
             sessionStorage.setItem("GodCardPlayer2Inactive", this.state.card1Inactive);
             sessionStorage.setItem("GodCardPlayer2", this.state.card1);
         }
-        if ((godCard === this.state.card2 || godCard === this.state.card2Inactive) && id === this.state.player2.id){
+        if ((godCard === this.state.card2 || godCard === this.state.card2Inactive) && id === this.state.player2.id) {
             sessionStorage.setItem("GodCardPlayer2Inactive", this.state.card2Inactive);
             sessionStorage.setItem("GodCardPlayer2", this.state.card2);
         }
-        if ((godCard === this.state.card3 || (godCard === "InactiveAthena")) && (id === this.state.player2.id)){
+        if ((godCard === this.state.card3 || (godCard === "InactiveAthena")) && (id === this.state.player2.id)) {
             sessionStorage.setItem("GodCardPlayer2Inactive", this.state.card3Inactive);
             sessionStorage.setItem("GodCardPlayer2", this.state.card3);
         }
-        if ((godCard === this.state.card4 || godCard === this.state.card4Inactive) && id === this.state.player2.id){
+        if ((godCard === this.state.card4 || godCard === this.state.card4Inactive) && id === this.state.player2.id) {
             sessionStorage.setItem("GodCardPlayer2Inactive", this.state.card4Inactive);
             sessionStorage.setItem("GodCardPlayer2", this.state.card4);
         }
-        if ((godCard === this.state.card5 || godCard === this.state.card5Inactive) && id === this.state.player2.id){
+        if ((godCard === this.state.card5 || godCard === this.state.card5Inactive) && id === this.state.player2.id) {
             sessionStorage.setItem("GodCardPlayer2Inactive", this.state.card5Inactive);
             sessionStorage.setItem("GodCardPlayer2", this.state.card5);
         }
-        if ((godCard === this.state.card6 || godCard === this.state.card6Inactive) && id === this.state.player2.id){
+        if ((godCard === this.state.card6 || godCard === this.state.card6Inactive) && id === this.state.player2.id) {
             sessionStorage.setItem("GodCardPlayer2Inactive", this.state.card6Inactive);
             sessionStorage.setItem("GodCardPlayer2", this.state.card6);
         }
-        if ((godCard === this.state.card7 || godCard === this.state.card7Inactive) && id === this.state.player2.id){
+        if ((godCard === this.state.card7 || godCard === this.state.card7Inactive) && id === this.state.player2.id) {
             sessionStorage.setItem("GodCardPlayer2Inactive", this.state.card7Inactive);
             sessionStorage.setItem("GodCardPlayer2", this.state.card7);
         }
-        if ((godCard === this.state.card8 || godCard === this.state.card8Inactive) && id === this.state.player2.id){
+        if ((godCard === this.state.card8 || godCard === this.state.card8Inactive) && id === this.state.player2.id) {
             sessionStorage.setItem("GodCardPlayer2Inactive", this.state.card8Inactive);
             sessionStorage.setItem("GodCardPlayer2", this.state.card8);
         }
-        if ((godCard === this.state.card9 || godCard === this.state.card9Inactive) && id === this.state.player2.id){
+        if ((godCard === this.state.card9 || godCard === this.state.card9Inactive) && id === this.state.player2.id) {
             sessionStorage.setItem("GodCardPlayer2Inactive", this.state.card9Inactive);
             sessionStorage.setItem("GodCardPlayer2", this.state.card9);
         }
-        if ((godCard === this.state.card10 || godCard === this.state.card10Inactive) && id === this.state.player2.id){
+        if ((godCard === this.state.card10 || godCard === this.state.card10Inactive) && id === this.state.player2.id) {
             sessionStorage.setItem("GodCardPlayer2Inactive", this.state.card10Inactive);
             sessionStorage.setItem("GodCardPlayer2", this.state.card10);
         }
 
+    }
+
+    darkenGodCard(n, card) {
+        if ((this.state.chosenCardPlayer1 === (null || "None") || this.state.chosenCardPlayer2 === (null || "None"))) {
+            return ("godCard" + n.toString());
+        } else if ((this.state.chosenCardPlayer1 === card) || (this.state.chosenCardPlayer2 === card)) {
+            return "godCard" + n.toString();
+        } else {
+            return "godCard" + n.toString() + "-dark";
+    }
     }
 
 
@@ -309,50 +309,60 @@ class ChooseGodCard extends React.Component {
                 </link>
                 <h1 className="chooseGodCard-h1">{this.state.alertText}</h1>
                 <div className="first-row">
-                    <div class="godCard1" id={this.getBorderSelected("Apollo")} onClick={() => {
-                        this.choose_card(this.state.card1, this.state.card1Inactive)
-                    }}></div>
-                    <div class="godCard2" id={this.getBorderSelected(this.state.card2Inactive)} onClick={() => {
+                    <div class={this.darkenGodCard(1, "Apollo")} id={this.getBorderSelected("Apollo")}
+                         onClick={() => {
+                             this.choose_card(this.state.card1, this.state.card1Inactive)
+                         }}></div>
+                    <div class={this.darkenGodCard(2, this.state.card2Inactive)}
+                         id={this.getBorderSelected(this.state.card2Inactive)} onClick={() => {
                         this.choose_card(this.state.card2, this.state.card2Inactive)
                     }}></div>
-                    <div class="godCard3" id={this.getBorderSelected("InactiveAthena")} onClick={() => {
+                    <div class={this.darkenGodCard(3, "InactiveAthena")}
+                         id={this.getBorderSelected("InactiveAthena")} onClick={() => {
                         this.choose_card(this.state.card3, this.state.card3Inactive)
                     }}></div>
-                    <div class="godCard4" id={this.getBorderSelected(this.state.card4Inactive)} onClick={() => {
+                    <div class={this.darkenGodCard(4, this.state.card4Inactive)}
+                         id={this.getBorderSelected(this.state.card4Inactive)} onClick={() => {
                         this.choose_card(this.state.card4, this.state.card4Inactive)
                     }}></div>
-                    <div class="godCard5" id={this.getBorderSelected(this.state.card5Inactive)} onClick={() => {
+                    <div class={this.darkenGodCard(5, this.state.card5Inactive)}
+                         id={this.getBorderSelected(this.state.card5Inactive)} onClick={() => {
                         this.choose_card(this.state.card5, this.state.card5Inactive)
                     }}></div>
                 </div>
                 <div className="second-row">
-                    <div class="godCard6" id={this.getBorderSelected(this.state.card6Inactive)} onClick={() => {
+                    <div class={this.darkenGodCard(6, this.state.card6Inactive)}
+                         id={this.getBorderSelected(this.state.card6Inactive)} onClick={() => {
                         this.choose_card(this.state.card6, this.state.card6Inactive)
                     }}></div>
-                    <div class="godCard7" id={this.getBorderSelected(this.state.card7Inactive)} onClick={() => {
+                    <div class={this.darkenGodCard(7, this.state.card7Inactive)}
+                         id={this.getBorderSelected(this.state.card7Inactive)} onClick={() => {
                         this.choose_card(this.state.card7, this.state.card7Inactive)
                     }}></div>
-                    <div class="godCard8" id={this.getBorderSelected(this.state.card8)} onClick={() => {
+                    <div class={this.darkenGodCard(8, this.state.card8)}
+                         id={this.getBorderSelected(this.state.card8)} onClick={() => {
                         this.choose_card(this.state.card8, this.state.card8Inactive)
                     }}></div>
-                    <div class="godCard9" id={this.getBorderSelected(this.state.card9)} onClick={() => {
+                    <div class={this.darkenGodCard(9, this.state.card9)}
+                         id={this.getBorderSelected(this.state.card9)} onClick={() => {
                         this.choose_card(this.state.card9, this.state.card9Inactive)
                     }}></div>
-                    <div class="godCard10" id={this.getBorderSelected(this.state.card10Inactive)} onClick={() => {
+                    <div class={this.darkenGodCard(10, this.state.card10Inactive)}
+                         id={this.getBorderSelected(this.state.card10Inactive)} onClick={() => {
                         this.choose_card(this.state.card10, this.state.card10Inactive)
                     }}
                     ></div>
                 </div>
                 <div className="centerTheButton">
                     <ButtonContainer/>
-                    <Button_1 className = "rock_godCard-button"
-                        disabled={this.state.gameStatus === "Start" || this.state.gameStatus === null}
-                        width="30%"
-                        onClick={() => {
-                            this.setSessionStorageCard(this.state.player1.worker1.godCard, this.state.player1.id);
-                            this.setSessionStorageCard(this.state.player2.worker1.godCard, this.state.player2.id);
-                            this.props.history.push(`/game/${sessionStorage.getItem("gameID")}/gamePlay/GodMode`);
-                        }}
+                    <Button_1 className="rock_godCard-button"
+                              disabled={this.state.gameStatus === "Start" || this.state.gameStatus === null}
+                              width="30%"
+                              onClick={() => {
+                                  this.setSessionStorageCard(this.state.player1.worker1.godCard, this.state.player1.id);
+                                  this.setSessionStorageCard(this.state.player2.worker1.godCard, this.state.player2.id);
+                                  this.props.history.push(`/game/${sessionStorage.getItem("gameID")}/gamePlay/GodMode`);
+                              }}
                     >
                         Go to Playground
                     </Button_1>
